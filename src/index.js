1367bf032de164c6ab68a8d6f40994cd7da47733
Mathias Ose <mathias.ose@gmail.com>
2017-08-16 11:07:15 +0200

    Linting

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-var axios = require('axios');
+const axios = require('axios');
 
 module.exports = {
-    API_HOST: 'https://bot.kindly.ai',
-    API_KEY: null,
-    send: function send(data) {
-        return axios.post(this.API_HOST + '/api/v1/send', {
-            api_key:        this.API_KEY,
-            user_id:        data.user_id,
-            message:        data.message,
-            exchange_id:    data.exchange_id,
-      });
-    }
-}
+  API_HOST: 'https://bot.kindly.ai',
+  API_KEY: null,
+  send: function send(data) {
+    return axios.post(`${this.API_HOST}/api/v1/send`, {
+      api_key: this.API_KEY,
+      user_id: data.user_id,
+      message: data.message,
+      exchange_id: data.exchange_id,
+    });
+  },
+};
